f75787f9c580c9631f60167d4ac2456a091b0d56
Shripad <71135716+shripad-jyothinath@users.noreply.github.com>
2026-02-17 14:21:18 +0530

    Commit UC3

--- file_uc2.java
+++ file_uc2.java
@@ -1,10 +1,10 @@
 public class file_uc2 {
     public static void main(String[] args) {
-        System.out.println("    ***    "+ "    ***   " +"   "+ "******* " + "  ****** ");
-        System.out.println("  **   **  "+ "  **   ** " +"   "+"**    **" + " ***     ");
-        System.out.println(" **     ** "+ " **     **" +"   "+"**   ** " + "   ***** ");
-        System.out.println(" **     ** "+ " **     **" +"   "+"** **   " + "      ***");
-        System.out.println(" **     ** "+ " **     **" +"   "+"**      " + "    ***  ");
-        System.out.println("   ** **   "+ "   ** **  " +"   "+"**      " + " ***     "); 
+        System.out.println(String.join(" ", "    ***    ", "    ***   " ,"   ", "******* " , "  ****** "));
+        System.out.println(String.join(" ", "  **   **  ", "  **   ** " ,"   ","**    **" , " ***     "));
+        System.out.println(String.join(" ", " **     ** ", " **     **" ,"   ","**   ** " , "   ***** "));
+        System.out.println(String.join(" ", " **     ** ", " **     **" ,"   ","** **   " , "      ***"));
+        System.out.println(String.join(" ", " **     ** ", " **     **" ,"   ","**      " , "    ***  "));
+        System.out.println(String.join(" ", "   ** **   ", "   ** **  " ,"   ","**      " , " ***     ")); 
     }
 }
