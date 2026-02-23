0bb5525456cd0e89653d86651ef13285064b41a8
Shripad <71135716+shripad-jyothinath@users.noreply.github.com>
2026-02-23 13:43:37 +0530

    commit UC5

---
+++ file_uc5.java
@@ -0,0 +1,15 @@
+public class file_uc5 {
+    public static void main(String[] args) {
+        String[] lines = {
+        String.join(" ", "    ***    ", "    ***   " ,"   ", "******* " , "  ****** "),
+        String.join(" ", "  **   **  ", "  **   ** " ,"   ","**    **" , " ***     "),
+        String.join(" ", " **     ** ", " **     **" ,"   ","**   ** " , "   ***** "),
+        String.join(" ", " **     ** ", " **     **" ,"   ","** **   " , "      ***"),
+        String.join(" ", " **     ** ", " **     **" ,"   ","**      " , "    ***  "),
+        String.join(" ", "   ** **   ", "   ** **  " ,"   ","**      " , " ***     ")
+        };
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
