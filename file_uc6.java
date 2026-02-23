6e39b769ce529b7e8daae7b83a7c8033c31ff4eb
Shripad <71135716+shripad-jyothinath@users.noreply.github.com>
2026-02-23 14:15:55 +0530

    Add UC6

---
+++ file_uc6.java
@@ -0,0 +1,40 @@
+public class file_uc6 {
+    public static String[] getOpattern() {
+        return new String[] {
+            "    ***    ",
+            "  **   **  ",
+            " **     ** ",
+            " **     ** ",
+            " **     ** ",
+            "   ** **   "
+        };
+    }
+    public static String[] getSpattern() {
+        return new String[] {
+            "  ****** ",
+            " ***     ",
+            "   ***** ",
+            "      ***",
+            "    ***  ",
+            " ***     "
+        };
+    }
+    public static String[] getPpattern() {
+        return new String[] {
+            "******* ",
+            "**    **",
+            "**   ** ",
+            "** **   ",
+            "**      ",
+            "**      "
+        };
+    }
+    public static void main(String[] args) {
+        String[] oPattern = getOpattern();
+        String[] sPattern = getSpattern();
+        String[] pPattern = getPpattern();
+        for (int i = 0; i < oPattern.length; i++) {
+        System.out.println(oPattern[i] + " " + oPattern[i] + "" + pPattern[i] + " " + sPattern[i]);
+        }
+    }
+}
